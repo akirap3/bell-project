c88afd0880903484bdc5cc8d5fc59aae714a0c24
akirap3 <akirapf3@gmail.com>
2026-06-11 12:06:26 -0400

    feat: enhance NotFoundException with resource name and key properties

--- BellProject.Application/Exceptions/NotFoundException.cs
+++ BellProject.Application/Exceptions/NotFoundException.cs
@@ -4,8 +4,22 @@ namespace BellProject.Application.Exceptions
 {
     public class NotFoundException : Exception
     {
+        public string? ResourceName { get; }
+        public object? ResourceKey { get; }
+
         public NotFoundException(string message) : base(message)
         {
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+            ResourceName = name;
+            ResourceKey = key;
         }
     }
 }
